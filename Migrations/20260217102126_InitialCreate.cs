6af1878970273ee2be054ab9173b0ab25a170e6a
glogvinoff-tech <g.logvinoff@gmail.com>
2026-02-21 11:43:22 +0300

    Add files via upload

---
+++ Migrations/20260217102126_InitialCreate.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace LibraryManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Country = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Genres",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genres", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    PublishYear = table.Column<int>(type: "int", nullable: false),
+                    ISBN = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    QuantityInStock = table.Column<int>(type: "int", nullable: false, defaultValue: 0),
+                    AuthorId = table.Column<int>(type: "int", nullable: false),
+                    GenreId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Books_Authors_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Authors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Books_Genres_GenreId",
+                        column: x => x.GenreId,
+                        principalTable: "Genres",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Authors",
+                columns: new[] { "Id", "BirthDate", "Country", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(1828, 9, 9, 0, 0, 0, 0, DateTimeKind.Unspecified), "Россия", "Лев", "Толстой" },
+                    { 2, new DateTime(1821, 11, 11, 0, 0, 0, 0, DateTimeKind.Unspecified), "Россия", "Федор", "Достоевский" },
+                    { 3, new DateTime(1860, 1, 29, 0, 0, 0, 0, DateTimeKind.Unspecified), "Россия", "Антон", "Чехов" },
+                    { 4, new DateTime(1799, 6, 6, 0, 0, 0, 0, DateTimeKind.Unspecified), "Россия", "Александр", "Пушкин" },
+                    { 5, new DateTime(1891, 5, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), "Россия", "Михаил", "Булгаков" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Genres",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Художественное произведение большого объема", "Роман" },
+                    { 2, "Произведение о расследовании преступлений", "Детектив" },
+                    { 3, "Произведение о вымышленных мирах", "Фантастика" },
+                    { 4, "Стихотворные произведения", "Поэзия" },
+                    { 5, "Описание жизни человека", "Биография" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "AuthorId", "GenreId", "ISBN", "PublishYear", "QuantityInStock", "Title" },
+                values: new object[,]
+                {
+                    { 1, 1, 1, "978-5-17-123456-7", 1869, 10, "Война и мир" },
+                    { 2, 1, 1, "978-5-17-123457-4", 1877, 8, "Анна Каренина" },
+                    { 3, 2, 1, "978-5-17-123458-1", 1866, 7, "Преступление и наказание" },
+                    { 4, 2, 1, "978-5-17-123459-8", 1869, 5, "Идиот" },
+                    { 5, 3, 4, "978-5-17-123460-4", 1904, 6, "Вишневый сад" },
+                    { 6, 4, 1, "978-5-17-123461-1", 1833, 9, "Евгений Онегин" },
+                    { 7, 5, 3, "978-5-17-123462-8", 1967, 12, "Мастер и Маргарита" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Authors_LastName_FirstName",
+                table: "Authors",
+                columns: new[] { "LastName", "FirstName" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_AuthorId",
+                table: "Books",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_GenreId",
+                table: "Books",
+                column: "GenreId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_Title",
+                table: "Books",
+                column: "Title");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Genres_Name",
+                table: "Genres",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Authors");
+
+            migrationBuilder.DropTable(
+                name: "Genres");
+        }
+    }
+}
